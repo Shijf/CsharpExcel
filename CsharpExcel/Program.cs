4990b9604aef931cfbad409d23a558a1b79b51de
hefju <hefju@qq.com>
2016-05-11 16:43:30 +0800

    导出到excel, 未测试

--- CsharpExcel/Program.cs
+++ CsharpExcel/Program.cs
@@ -16,7 +16,7 @@ namespace CsharpExcel
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmReader());
+            Application.Run(new FrmWriter());
         }
     }
 }
